80bbfba4b22bb6f54b94d8bf8f3204367105d48a
Michael <michael.jt.pro@gmail.com>
2026-01-27 23:55:03 +0100

    chore: inicializar proyecto Spring Boot + Docker + MySQL en carpeta backend

---
+++ backend/src/main/java/com/example/ResourcesHub/ResourcesHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResourcesHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesHubApplication.class, args);
+	}
+
+}
